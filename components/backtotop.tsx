7ff3826f223c0c5891519425fb1e50cf6d4d0c4f
yoshinorin <yoshinorin.net@outlook.com>
2021-07-30 23:36:04 +0900

    feat: implement back to top

---
+++ components/backtotop.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/backtotop.module.scss';
+
+export default function BackToTop() {
+  return (
+    <p id={styles['back-to-top']}>
+      <a href="#">
+        <svg viewBox="0 0 24 24">
+          <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/>
+        </svg>
+      </a>
+    </p>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import BackToTop from '../components/backtotop';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <BackToTop />
       <Footer />
     </>
   )
