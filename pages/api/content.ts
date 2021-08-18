e19eb50bb26c848e16f3999e14befe3829cdbae6
yoshinorin <yoshinorin.net@outlook.com>
2021-08-18 23:26:04 +0900

    feat: delete path startsWith slash check
    * API has been changed

--- pages/api/content.ts
+++ pages/api/content.ts
@@ -1,10 +1,6 @@
 import { api } from '../../config';
 
 export async function findByPath(path: string): Promise<Response> {
-  // TODO: move to util func and write tests code.
-  if (path.startsWith("/")) {
-    path = path.substr(1);
-  }
   if (!path.endsWith("/")) {
     path = path + "/"
   }
