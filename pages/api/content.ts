ac3c85baf5bce3fdddef39cf1230ecfc3cff376a
yoshinorin <yoshinorin.net@outlook.com>
2021-08-21 17:57:01 +0900

    fix: substr first of path if it is a slash

--- pages/api/content.ts
+++ pages/api/content.ts
@@ -1,6 +1,9 @@
 import { api } from '../../config';
 
 export async function findByPath(path: string): Promise<Response> {
+  if (path.startsWith("/")) {
+    path = path.substr(1, path.length)
+  }
   if (!path.endsWith("/")) {
     path = path + "/"
   }
