d7c6840aa1270fb8efce42814f0fce0ef51b6fb8
yoshinorin <yoshinorin.net@outlook.com>
2022-01-09 19:59:30 +0900

    style(tags): apply flex to tags section

--- components/tags.tsx
+++ components/tags.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 import { Tag } from '../types/tag';
 import containerStyles from '../styles/components/container.module.scss';
-import tagStyles from '../styles/components/tags.module.scss';
+import style from '../styles/tags.module.scss';
 
 const TagsComponent: React.FunctionComponent<{ tags: Array<Tag> }> = ({ tags }) => {
   return (
-    <section className={`${containerStyles.container}`}>
+    <section className={`${containerStyles['flex-container']}`}>
       {tags.map((t: Tag) => {
         return (
           <Link href={`/tags/${t.name}`}>
-            <a className={`${tagStyles.tag}`} data-tag={t.name.toLowerCase()}>{`${t.name}`}</a>
+            <a className={`${style.tag}`} data-tag={t.name.toLowerCase()}>{`${t.name}`}</a>
           </Link>
         )
       })}
