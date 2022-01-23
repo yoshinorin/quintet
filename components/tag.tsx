3d41c9f3775bc037672168cc32b53d384fe6047d
yoshinorin <yoshinorin.net@outlook.com>
2022-01-23 14:44:01 +0900

    refactor: create TagCompnent and apply it

---
+++ components/tag.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { Tag } from '../types/tag';
+
+const TagComponent: React.FunctionComponent<{ tag: Tag, className: string | null }> = ({ tag, className }) => {
+    {/*
+    NOTE:
+      The Next.js can not pass custom argument with <Link> component.
+      So, I want to belows, but can not...
+
+      Front-end visible URL: https://example.com/tags/{tagName}
+      API call (when transition with <Link>):  https://example.com/tags/{tagId}
+
+      But, it can not. So, I have to find the tagging contents with tagName.
+    */}
+    return (
+      <Link
+        href={`/tags/${tag.name}`}
+      >
+        <a
+          className={className ? className : null}
+          target="_blank"
+          data-tag={tag.name.toLowerCase()}
+        >
+          {`${tag.name}`}
+        </a>
+      </Link>
+    )
+}
+
+export default TagComponent;
--- components/tags.tsx
+++ components/tags.tsx
@@ -1,36 +1,18 @@
-import Link from 'next/link';
+import TagComponent from './tag';
 import { Tag } from '../types/tag';
 import containerStyles from '../styles/components/container.module.scss';
 import style from '../styles/tags.module.scss';
 
-
 const TagsComponent: React.FunctionComponent<{ tags: Array<Tag> }> = ({ tags }) => {
   return (
     <section className={`${containerStyles['flex-container']}`}>
-      {tags.map((t: Tag) => {
-        {/*
-        NOTE:
-          The Next.js can not pass custom argument with <Link> component.
-          So, I want to belows, but can not...
-
-          Front-end visible URL: https://example.com/tags/{tagName}
-          API call (when transition with <Link>):  https://example.com/tags/{tagId}
-
-          But, it can not. So, I have to find the tagging contents with tagName.
-        */}
-        return (
-          <Link
-            href={`/tags/${t.name}`}
-            key={t.id}
-          >
-            <a
-              className={`${style.tag}`}
-              target="_blank"
-              data-tag={t.name.toLowerCase()}
-            >
-              {`${t.name}`}
-            </a>
-          </Link>
+      {tags.map((tag: Tag) => {
+        return(
+          <TagComponent
+            tag={tag}
+            className={`${style['tag']}`}
+            key={tag.id}
+          />
         )
       })}
     </section>
