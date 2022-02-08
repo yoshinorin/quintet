eaf69d22e51a942a229ca651a156a413dac271a4
yoshinorin <yoshinorin.net@outlook.com>
2022-02-08 23:50:21 +0900

    fix: return current date for fallback when unixtime convert error

--- utils/time.ts
+++ utils/time.ts
@@ -1,9 +1,17 @@
 export function convertUnixtimeToDate(unixTime: number): Date {
-  return new Date(unixTime * 1000);
+  try {
+    return new Date(unixTime * 1000);
+  } catch {
+    return new Date();
+  }
 }
 
 export function convertUnixTimeToISODateSrting(unixTime: number): string {
-  return new Date(unixTime * 1000).toISOString();
+  try {
+    return new Date(unixTime * 1000).toISOString();
+  } catch {
+    return new Date().toISOString();
+  }
 }
 
 // TODO: refactor naming
