b41a7832a45ab37c4225214bc0e289dba3e0cb0d
yoshinorin <yoshinorin.net@outlook.com>
2022-03-06 22:43:12 +0900

    fix: more quard

--- utils/filterRequests.ts
+++ utils/filterRequests.ts
@@ -2,7 +2,7 @@ import { isMatch } from 'micromatch';
 import { filterRequestPaths } from '../config';
 
 export function isIgnoreRequest(path: string) {
-  if (!path || filterRequestPaths.length == 0) {
+  if (!path || !filterRequestPaths || filterRequestPaths.length == 0) {
     return false;
   }
   return isMatch(path, filterRequestPaths);
