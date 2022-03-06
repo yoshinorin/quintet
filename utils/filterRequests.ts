c718ea689403e18e9a0e0d13dc33c1062f5542ba
yoshinorin <yoshinorin.net@outlook.com>
2022-03-06 22:38:59 +0900

    fix: add null guard

--- utils/filterRequests.ts
+++ utils/filterRequests.ts
@@ -2,5 +2,8 @@ import { isMatch } from 'micromatch';
 import { filterRequestPaths } from '../config';
 
 export function isIgnoreRequest(path: string) {
+  if (!path || filterRequestPaths.length == 0) {
+    return false;
+  }
   return isMatch(path, filterRequestPaths);
 }
