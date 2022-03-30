19cbaebda3483325a832fed45526bcfefee3dcce
yoshinorin <yoshinorin.net@outlook.com>
2022-03-30 21:07:15 +0900

    chore(deps): bump next.js from 12.0.10 to 12.1.1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'quintet',
-      cwd: '.next/',
+      // cwd: '.next/',
       script: 'npm',
+      // script: './node_modules/next/dist/bin/next',
       args: 'start',
       exec_mode: 'cluster',
-      instances: 2
+      // exec_mode: 'fork',
+      instances: 2,
+      // exp_backoff_restart_delay: 10
     }
 ],};
