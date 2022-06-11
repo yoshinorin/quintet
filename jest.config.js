c8fbc35a737c994888c82508a37b35755ba39fcc
yoshinorin <yoshinorin.net@outlook.com>
2022-06-11 10:40:47 +0900

    test: ignore `jest-environment-jsdom`

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: 'jest-environment-jsdom',
 }
 
 module.exports = createJestConfig(customJestConfig)
