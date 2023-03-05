574f6cf610a8af563c492a31fa665ab044462cc9
yoshinorin <yoshinorin.net@outlook.com>
2023-03-06 04:52:32 +0900

    chore(pagination): add `FIX` comment

--- services/pagination.ts
+++ services/pagination.ts
@@ -1,6 +1,7 @@
 import { PaginationNumbers } from '../models/pagination';
 
 export function calcNumberOfPages(total: number, perPage: number): PaginationNumbers {
+  // FIX: do not add +1 to last page
   const last = Math.ceil(total / perPage) + 1;
   const pages = [...Array(last)].map((_, i) => i);
   pages.shift();
