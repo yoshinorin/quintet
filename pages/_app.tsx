35bee3f716b7a433a5888e363f972e09d818e7c3
yoshinorin <yoshinorin.net@outlook.com>
2023-03-26 00:49:33 +0900

    feat(theme): load theme settings from local storage in `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import '../styles/globals.scss';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import HeaderComponent from '../components/header';
 import FooterComponent from '../components/footer';
 import BackToTopComponent from '../components/backtotop';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    let theme = 'light';
+    try {
+      theme = localStorage.getItem('theme');
+    } catch(e) {
+      // Nothing to do
+    };
+    const body = document.body;
+    body.setAttribute('data-theme', theme);
+  });
   return (
     <>
       <HeaderComponent />
