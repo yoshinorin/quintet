fe4af19b6e41b44c6797bffef8e9d4cef47692dd
yoshinorin <yoshinorin.net@outlook.com>
2023-03-29 23:26:52 +0900

    feat(component): implement `DropdownComponent`

---
+++ components/dropdown.tsx
@@ -0,0 +1,18 @@
+const DropdownComponent: React.FunctionComponent<{
+  list: Array<string>,
+  defaultValue: string | null,
+  onChange: any
+}> = ({ list, defaultValue, onChange }) => {
+
+  return (
+    <select value={defaultValue} onChange={onChange}>
+      {list.map(x => (
+        <option key={x} value={x}>
+          {x}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default DropdownComponent;
