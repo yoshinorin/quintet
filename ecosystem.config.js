15e153f50d47fba257c8a6fb8c34432e2c76d2c2
yoshinorin <yoshinorin.net@outlook.com>
2023-05-26 20:07:51 +0900

    chore(pm2): reduce pm2 cluster instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,10 @@ module.exports = {
       args: 'start',
       exec_mode: 'cluster',
       // exec_mode: 'fork',
-      instances: 2,
+
+      // NOTE: When trying to launch two instances, one of them allways restarting.
+      // instances: 2,
+      instances: 1,
       // exp_backoff_restart_delay: 10
     }
 ],};
