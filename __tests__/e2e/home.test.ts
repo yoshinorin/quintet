f95f29bc16c9e95677a0caf8f1be6f188811fdda
yoshinorin <yoshinorin.net@outlook.com>
2023-12-30 17:49:23 +0900

    test(e2e): add root page head meta e2e tests

--- __tests__/e2e/home.test.ts
+++ __tests__/e2e/home.test.ts
@@ -1,6 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  // TODO
+});
+
+test.describe('Display Home', () => {
+  /* FIXME: this test case always passes.
+  test('navbar-items are exists', async ({ page }) => {
+    await expect(page.getByTitle('Home')).toBeVisible;
+    await expect(page.getByTitle('Archives')).toBeVisible;
+  });
+  */
+
+  test('should exists and set collect values in head meta', async ({ page }) => {
+    await expect(page.locator('meta[name="author"]')).toHaveAttribute('content', 'john doe');
+    await expect(page.locator('meta[property="article:author"]')).toHaveAttribute('content', 'john doe');
+    await expect(page.locator('meta[property="og:title"]')).toHaveAttribute('content', 'E2E Test Site');
+    await expect(page.locator('meta[property="og:type"]')).toHaveAttribute('content', 'blog');
+    await expect(page.locator('meta[property="og:url"]')).toHaveAttribute('content', 'http://localhost:3000/');
+    await expect(page.locator('meta[property="og:site_name"]')).toHaveAttribute('content', 'E2E Test Site');
+    await expect(page.locator('meta[property="og:locale"]')).toHaveAttribute('content', 'ja_JP');
+    await expect(page.locator('meta[property="og:image"]')).toHaveAttribute('content', 'defaultImage.jpg');
+    await expect(page.locator('meta[name="robots"]')).toHaveAttribute('content', 'noarchive, nofollow, noimageindex, noindex');
+    await expect(page.locator('meta[name="injectedMetaName"]')).toHaveAttribute('content', 'injectedMetaContent');
+  });
 });
