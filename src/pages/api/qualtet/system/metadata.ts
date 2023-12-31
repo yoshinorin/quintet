8afcf83bafb3bd9880f493058d8374b8c4fbe161
yoshinorin <yoshinorin.net@outlook.com>
2023-12-31 16:41:46 +0900

    refactor: specify `GET` method explicitly when request `/system/metadata`

--- src/pages/api/qualtet/system/metadata.ts
+++ src/pages/api/qualtet/system/metadata.ts
@@ -1,5 +1,7 @@
 import { publicApi } from '../../../../../config';
 
 export default async function GET() {
-  return fetch(`${publicApi.url}/system/metadata`)
+  return fetch(`${publicApi.url}/system/metadata`, {
+    method: 'GET'
+  });
 }
