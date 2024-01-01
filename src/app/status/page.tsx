09f711f6551559b55cfe224213c36bb55de8fceb
yoshinorin <yoshinorin.net@outlook.com>
2024-01-01 22:34:57 +0900

    update

---
+++ src/app/status/page.tsx
@@ -0,0 +1,20 @@
+import { headers } from 'next/headers';
+
+import { getStatus } from '../../api/status';
+import { getRequestContext } from '../../utils/requestContext';
+import Renderer from './renderer';
+
+export default async function Page() {
+  const { props } = await get();
+  return <Renderer {...props} />
+}
+
+export async function get() {
+  const response: Response = await getStatus(getRequestContext(headers()));
+
+  return {
+    props: {
+      statusCode: response.status
+    }
+  }
+}
