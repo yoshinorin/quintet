ac151f4dc5f4bcd79801dab2aaf3214656d61ae0
yoshinorin <yoshinorin.net@outlook.com>
2024-01-02 16:44:46 +0900

    update

--- src/app/tags/renderer.tsx
+++ src/app/tags/renderer.tsx
@@ -1,13 +1,15 @@
+'use client';
+
 import HeadMetaComponent from '../../components/headmeta';
 import CoverWithNavigationComponent from '../../components/cover/withNavigation';
 import TagsComponent from '../../components/tags';
-import { Tag } from '../../models/tag';
 import { defaultRobotsMeta } from '../../../config';
-import { getTags } from '../../api/tags';
-import { getRequestContext } from '../../utils/requestContext';
 import PlanePage from '../../components/planePage';
 
-export default function Page({ statusCode, tags }) {
+export const Renderer: React.FunctionComponent<{
+  statusCode,
+  tags
+}> = ({ statusCode, tags }) => {
   if (statusCode !== 200) {
     return <PlanePage
       title={statusCode.toString()}
@@ -34,21 +36,4 @@ export default function Page({ statusCode, tags }) {
       </main>
     </>
   )
-}
-
-export async function getServerSideProps(ctx: any) {
-  const response: Response = await getTags(getRequestContext(ctx.req));
-  ctx.res.statusCode = response.status;
-
-  let tags: Array<Tag> = [];
-  if (response.status === 200) {
-    tags = await response.json() as Array<Tag>;
-  }
-
-  return {
-    props: {
-      statusCode: response.status,
-      tags: tags
-    }
-  }
 }
