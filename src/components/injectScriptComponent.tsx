5e244f0f2dc47262fe216519a1ded5efa87bcb17
yoshinorin <yoshinorin.net@outlook.com>
2024-01-05 19:37:02 +0900

    update

--- src/components/injectScriptComponent.tsx
+++ src/components/injectScriptComponent.tsx
@@ -1,34 +1,31 @@
 'use client';
 
-// import Script from 'next/script'
 import Script from 'next/script'
 import { InjectScript } from '../models/models';
 
 export const InjectScriptComponent: React.FunctionComponent<{
-  injectScript: Array<InjectScript>
-}> = ({ injectScript }) => {
-  if (injectScript.length === 0) {
+  injectScripts: Array<InjectScript>
+}> = ({ injectScripts }) => {
+  if (injectScripts.length === 0) {
     return;
   }
 
   return(
     <>
-      {injectScript.map((tag: InjectScript) => {
+      {injectScripts.map((s: InjectScript) => {
         return(
-          <>
-            <Script
-              key={tag.key}
-              src={tag.src}
-              // type={tag.code.type}
-              // TODO: more configulable.
-              onLoad={() => {
-                eval(tag.code.code)
-              }}
-              // TODO: more configulable.
-              strategy='lazyOnload'
-            >
-            </Script>
-          </>
+          <Script
+            key={s.key}
+            src={s.src}
+            // type={tag.code.type}
+            // TODO: more configulable.
+            onLoad={() => {
+              eval(s.code.code)
+            }}
+            // TODO: more configulable.
+            strategy='lazyOnload'
+          >
+          </Script>
         )
       })}
     </>
