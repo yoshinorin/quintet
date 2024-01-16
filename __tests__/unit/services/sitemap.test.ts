5fa575a1a01278921a8029e04581890c2d19904b
yoshinorin <yoshinorin.net@outlook.com>
2024-01-16 23:17:39 +0900

    test(sitemap): add test for `sitemap.xml` string generator

---
+++ __tests__/unit/services/sitemap.test.ts
@@ -0,0 +1,40 @@
+import { expect, test } from 'vitest'
+import { generateSitemapString } from '../../../src/services/sitemap';
+import { Sitemap } from '../../../src/models/sitemap';
+
+test('generate sitemap.xml', async () => {
+
+  const data: Array<Sitemap> = [
+    {
+      loc: '/articles/abc',
+      lastMod: '2020-01-01'
+    },
+    {
+      loc: '/articles/def',
+      lastMod: '2022-02-02'
+    },
+    {
+      loc: '/diary/',
+      lastMod: '2023-03-03'
+    }
+  ]
+
+  const result = await generateSitemapString('https://example.com', data);
+  expect(result.replace(/\s/g,"")).toEqual(
+    `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+    <loc>https://example.com/articles/abc</loc>
+    <lastmod>2020-01-01</lastmod>
+    </url>
+    <url>
+    <loc>https://example.com/articles/def</loc>
+    <lastmod>2022-02-02</lastmod>
+    </url>
+    <url>
+    <loc>https://example.com/diary/</loc>
+    <lastmod>2023-03-03</lastmod>
+    </url>
+    </urlset>
+    `.replace(/\s/g,"")
+  );
+});
