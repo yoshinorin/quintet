736c521362b5f6428b623d6b6bb3376af8cfb8fe
yoshinorin <yoshinorin.net@outlook.com>
2024-01-18 01:29:07 +0900

    test: excludes some files for coverage

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,18 @@ export default defineConfig({
     // globals: true
     include: [
       './__tests__/unit/**/*.{test,spec}.?(c|m)[jt]s?(x)'
-    ]
+    ],
+    coverage: {
+      exclude: [
+        '**/*.tsx',
+        '.next/*',
+        'config.js',
+        'config.*.js',
+        '*.config.js',
+        'next-env.d.ts',
+        '**/models/*',      // Just a interface.
+        // '**/api/*',      // Need a mock. Coverd with E2E testing.
+      ],
+    }
   },
 })
