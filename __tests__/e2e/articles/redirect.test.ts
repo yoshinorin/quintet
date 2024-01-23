f9a42fad3430598c942d03e8b6659023b023e666
yoshinorin <yoshinorin.net@outlook.com>
2024-01-23 23:15:12 +0900

    test(e2e/articles): add redirect test cases

---
+++ __tests__/e2e/articles/redirect.test.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Article - Redirect', () => {
+  test('should be redirect from `/{yyyy}/{mm}/{dd}/` to `/articles/{yyyy}/{mm}/{dd}/` and set collect values in head meta', async ({ page } ) => {
+
+    await page.goto('http://localhost:3000/2024/01/01/standard/');
+    await expect(page.url()).toBe('http://localhost:3000/articles/2024/01/01/standard/');
+
+    await expect(page.locator('meta[name="author"]')).toHaveAttribute('content', 'yoshinorin');
+    await expect(page.locator('meta[property="article:author"]')).toHaveAttribute('content', 'yoshinorin');
+    await expect(page.locator('meta[property="og:title"]')).toHaveAttribute('content', 'YYYY MM DD post');
+    await expect(page.locator('meta[property="og:type"]')).toHaveAttribute('content', 'blog');
+    await expect(page.locator('meta[property="og:url"]')).toHaveAttribute('content', 'http://localhost:3000/articles/2024/01/01/standard/');
+    await expect(page.locator('meta[property="og:site_name"]')).toHaveAttribute('content', 'E2E Test Site');
+    await expect(page.locator('meta[property="og:locale"]')).toHaveAttribute('content', 'ja_JP');
+    await expect(page.locator('meta[property="og:image"]')).toHaveAttribute('content', 'defaultImage.jpg');
+    await expect(page.locator('meta[name="injectedMetaName"]')).toHaveAttribute('content', 'injectedMetaContent');
+    await expect(page.locator('meta[name="robots"]')).toHaveAttribute('content', 'noarchive, nofollow, noimageindex, noindex');
+    await expect(page.locator('meta[name="description"]')).toHaveAttribute('content', 'Proin tellus nibh, pretium vitae bibendum in, tempus nec odio...');
+    await expect(page.locator('meta[property="og:description"]')).toHaveAttribute('content', 'Proin tellus nibh, pretium vitae bibendum in, tempus nec odio...');
+
+    // TODO: assert content value
+    await expect(page.locator('meta[property="article:published_time"]')).toHaveAttribute('content');
+    await expect(page.locator('meta[property="article:modified_time"]')).toHaveAttribute('content');
+
+    const tags = await page.locator('meta[property="article:tag"]').all();
+    await expect(tags[0]).toHaveAttribute('content', 'Scala');
+    await expect(tags[1]).toHaveAttribute('content', 'Cats');
+  });
+
+  test('should be redirect from `/{yyyy}/{mm}/{dd}` to `/articles/{yyyy}/{mm}/{dd}/` and set collect values in head meta', async ({ page } ) => {
+    await page.goto('http://localhost:3000/2024/01/01/standard');
+    await expect(page.url()).toBe('http://localhost:3000/articles/2024/01/01/standard/');
+  });
+
+  test('returns 500 if url not ends with `/{yyyy}/{mm}/{dd}/`: pattern1', async ({ page } ) => {
+    const response = await page.goto('http://localhost:3000/aaa/01/01/standard');
+    // FIXME: Next.js returns 500
+    await expect(response.status()).toBe(404);
+  });
+
+  test('returns 500 if url not ends with `/{yyyy}/{mm}/{dd}/`: pattern2', async ({ page } ) => {
+    const response = await page.goto('http://localhost:3000/2024/aa/01/standard');
+    // FIXME: Next.js returns 500
+    await expect(response.status()).toBe(404);
+  });
+
+  test('returns 500 if url not ends with `/{yyyy}/{mm}/{dd}/`: pattern3', async ({ page } ) => {
+    const response = await page.goto('http://localhost:3000/2024/01/bb/standard');
+    // FIXME: Next.js returns 500
+    await expect(response.status()).toBe(404);
+  });
+});
+
