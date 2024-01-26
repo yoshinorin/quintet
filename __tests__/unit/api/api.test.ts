bde0ea1051b1131a3ffcab1724e4924f3c4ea832
yoshinorin <yoshinorin.net@outlook.com>
2024-01-27 00:37:41 +0900

    test(api): add intercept testing

---
+++ __tests__/unit/api/api.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { fetchFromApi } from "../../../src/api/request";
+
+
+test('should returns 404 if path contains ignore chars', async () => {
+  const response = await fetchFromApi('https://example.com/hoge.svg', '', null, {
+    interceptIfContainsIgnorePaths: true
+  });
+  expect(response.status).toEqual(404);
+});
