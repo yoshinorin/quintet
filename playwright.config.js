fd03f99145c6fcf509d664b403c5b443060b3da9
yoshinorin <yoshinorin.net@outlook.com>
2024-01-30 21:47:12 +0900

    test(config): change destination dir of reports

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,12 @@ export default defineConfig({
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    [
+      'html',
+      { outputFolder: './report/e2e' }
+    ]
+  ],
   projects: [
     {
       name: 'chromium',
--- vitest.config.js
+++ vitest.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     ],
     coverage: {
       reporter: ['text', 'html'],
+      reportsDirectory: './report/coverage',
       exclude: [
         '**/*.tsx',
         '.next/*',
