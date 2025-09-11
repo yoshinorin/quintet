1af17e6d448cbf40b8108c3099dae3c9fd7219a5
yoshinorin <yoshinorin.net@outlook.com>
2025-09-12 08:37:07 +0900

    chore(logger): explicitly initialize the logger in the `instrumentation.ts`

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,9 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    // NOTE: Explicitly initialize the logger here.
+    const { logger } = await import("./utils/logger");
+    logger.info({ message: "logger initialized" });
+
     await import("./otel/lifecycle").then(({ initializeOtel }) =>
       initializeOtel()
     );
